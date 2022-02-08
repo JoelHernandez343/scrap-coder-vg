e8c5ce4c8c5f4dbaa29ff158c60180ad4b60b8d0
JoelHernandez343 <joel.programador@gmail.com>
2022-02-07 23:02:35 -0600

    Added RepeatBehaviour for specific behaviour

---
+++ Assets/Scripts/Nodes/CustomBehaviour/Repeat/RepeatBehaviour.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBehaviour : MonoBehaviour, INodeSelectorModifier, INodePositioner, IZoneParentRefresher {
+
+    [SerializeField] NodeController controller;
+
+    [SerializeField] NodeTransform edgePiece;
+
+    [SerializeField] NodeZone childrenZone;
+
+    [SerializeField] NodeArray children;
+    [SerializeField] NodeArray siblings;
+
+    void INodeSelectorModifier.ModifySelectorFunc() {
+        controller.selector[NodeZoneColor.Red, NodeZoneColor.Blue] = AddNodesToChildren;
+        controller.selector[NodeZoneColor.Yellow, NodeZoneColor.Green] = AddNodesToChildren;
+    }
+
+    void AddNodesToChildren(NodeZone inZone, NodeZone ownZone, NodeController toThisNode = null) {
+        if (ownZone == childrenZone || ownZone.controller.parentArray == children) {
+            children.AddNodes(inZone.controller, toThisNode ?? controller);
+        } else {
+            siblings.AddNodes(inZone.controller, toThisNode ?? controller);
+        }
+    }
+
+    void INodePositioner.SetPartsPosition(NodeArray toThisArray) {
+        if (toThisArray == children) {
+            var dy = (children.height + 8) - edgePiece.height;
+
+            if (children.Count == 0) {
+                dy += 8;
+            }
+
+            edgePiece.Expand(dy: dy);
+            siblings.ownTransform.SetPositionByDelta(dy: -dy);
+
+            controller.ownTransform.Expand(dy: dy);
+        }
+
+
+        controller.parentArray?.SetPartsPosition(controller);
+    }
+
+    void IZoneParentRefresher.SetZonesAsParent(NodeArray array) {
+        if (array == children) {
+            if (array.Count == 0) {
+                childrenZone.color = NodeZoneColor.Red;
+            } else {
+                childrenZone.color = NodeZoneColor.Yellow;
+            }
+        } else {
+            throw new System.ArgumentException($"This array {array.gameObject.name} is unknown");
+        }
+    }
+}
