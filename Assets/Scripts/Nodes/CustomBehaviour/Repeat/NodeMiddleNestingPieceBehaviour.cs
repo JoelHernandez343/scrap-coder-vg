5f73b3828fd76ca02375de09c610db7efcb625af
JoelHernandez343 <joel.programador@gmail.com>
2022-02-11 00:37:49 -0600

    Added general behaviour for nodes with nesting

---
+++ Assets/Scripts/Nodes/CustomBehaviour/Repeat/NodeMiddleNestingPieceBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeMiddleNestingPieceBehaviour : MonoBehaviour, INodeExpander {
+    [SerializeField] NodeTransform edgeShape;
+    [SerializeField] NodeTransform unionSprite;
+    [SerializeField] NodeTransform mainShape;
+    [SerializeField] NodeTransform bottomZone;
+    [SerializeField] NodeTransform children;
+    [SerializeField] NodeTransform text;
+    [SerializeField] new NodeTransform collider;
+
+    void INodeExpander.Expand(int dx, int dy) {
+        collider.Expand(dx, dy);
+        edgeShape.Expand(dx, dy);
+
+        text?.SetPositionByDelta(dy: -dy);
+        children?.SetPositionByDelta(dy: -dy);
+        mainShape?.SetPositionByDelta(dy: -dy);
+        bottomZone?.SetPositionByDelta(dy: -dy);
+        unionSprite?.SetPositionByDelta(dy: -dy);
+    }
+}
