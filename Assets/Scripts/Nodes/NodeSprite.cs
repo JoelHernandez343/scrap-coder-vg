785cad6caf3a9b3a4158a7285b234389887829ce
JoelHernandez343 <joel.programador@gmail.com>
2022-03-13 16:23:53 -0600

    Added random sprites functionality

--- Assets/Scripts/Nodes/NodeSprite.cs
+++ Assets/Scripts/Nodes/NodeSprite.cs
@@ -7,12 +7,43 @@ using UnityEngine;
 namespace ScrapCoder.VisualNodes {
 
     public class NodeSprite : MonoBehaviour {
+
+        // Editor variables
         [SerializeField] public NodeTransform ownTransform;
         [SerializeField] new SpriteRenderer renderer;
+
+        [SerializeField] List<Sprite> availableSprites;
+
+        // State Variables
         [SerializeField] bool hideable;
 
-        public void Hide() => ToggleRender(false);
-        public void Show() => ToggleRender(true);
+        // Lazy state variables
+        int? _selectedSprite;
+        int selectedSprite {
+            set { _selectedSprite = value; }
+            get {
+                _selectedSprite ??= random.NextRange(0, availableSprites.Count - 1);
+
+                return (int)_selectedSprite;
+            }
+        }
+
+        // Lazy and other variables
+        Utils.Random _random;
+        Utils.Random random {
+            get {
+                _random ??= new Utils.Random();
+                return _random;
+            }
+        }
+
+        // Methods
+        void Awake() {
+            renderer.sprite = availableSprites[selectedSprite];
+        }
+
+        void Hide() => ToggleRender(false);
+        void Show() => ToggleRender(true);
 
         public void ToggleRender(bool render) {
             if (hideable) {
