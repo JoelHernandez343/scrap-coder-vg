56c9c9c8f06ae484b6b2b849c57d8849ed9773c4
JoelHernandez343 <joel.programador@gmail.com>
2023-03-27 22:05:32 -0600

    Quit null exception on Respawn Position (maybe this sould be different, temporary fix)

--- Assets/RespawnPosition.cs
+++ Assets/RespawnPosition.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class RespawnPosition : MonoBehaviour
 {
     private Transform[] positions = new Transform[2];
-    
+
+    // Maybe this a possible fix:
+    [SerializeField] Transform copperTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        positions[0] = transform.GetChild(0).GetComponent<Transform>();
-        positions[1] = transform.GetChild(1).GetComponent<Transform>();
+        // positions[0] = transform.GetChild(0).GetComponent<Transform>();
+        // positions[1] = transform.GetChild(1).GetComponent<Transform>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            collision.transform.position = positions[0].position;
+            /*collision.transform.position = positions[0].position;
             GameObject.Find("Copper").transform.position = positions[1].position;
-            ResetPositionEvent.newPosition(true);
+            ResetPositionEvent.newPosition(true);*/
+            // ResetPosition(copper, new Vector3...);
         }
     }
 }
